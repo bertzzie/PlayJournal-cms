384fffef72a409963d190bda8ea4128ee8e1e086
Alex Xandra Albert Sim <bertzzie@gmail.com>
2012-08-12 19:20:44 +0700

    Simplify Markdown.getFirstParagraph

--- app/views/helper/Markdown.scala
+++ app/views/helper/Markdown.scala
@@ -3,8 +3,8 @@ package views.helper
 object Markdown {
     def getFirstParagraph(txt: String) = {
         val newLineIdx = txt.indexOf("\n") match {
-            case i: Int if i > 0  => i
-            case i: Int if i <= 0 => txt.length
+            case i: Int if i > 0 => i
+            case _               => txt.length
         }
 
         txt.substring(0, newLineIdx)
